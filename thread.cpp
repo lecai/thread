a100095d922a1f250ebd5bf5cd30a5564ea14eee
lecai <Admin@lecais-MacBook-Pro.local>
2013-11-21 18:03:38 +0800

    add impl
    add impl

--- thread.cpp
+++ thread.cpp
@@ -4,6 +4,7 @@
 
 
 #include "thread.h"
+#include "mythread.h"
 
 namespace thread{
     //linux 和 mac 版本
@@ -22,4 +23,155 @@ namespace thread{
     inline void Mutex::unlock() {
         pthread_mutex_unlock(&m_hMutex);
     }
+
+    /*
+     * RWLock
+     */
+    RWLock::RWLock() :rd_count(0),wr_count(0){
+        pthread_rwlock_init(&m_hMutex, NULL);
+    }
+
+    RWLock::~RWLock() {
+        pthread_rwlock_destroy(&m_hMutex);
+    }
+
+    inline void RWLock::rdlock() {
+        pthread_rwlock_rdlock(&m_hMutex);
+        ++rd_count;
+    }
+
+    inline void RWLock::wrlock() {
+        pthread_rwlock_wrlock(&m_hMutex);
+        ++wr_count;
+        ++rd_count;
+    }
+
+    inline void RWLock::unlock() {
+        pthread_rwlock_destroy(&m_hMutex);
+        --rd_count;
+    }
+
+    /*
+     *  Cond
+     */
+    Cond::Cond() {
+          ::pthread_cond_init(&m_hEvent, NULL);
+    }
+
+    Cond::~Cond() {
+        ::pthread_cond_destroy(&m_hEvent);
+    }
+
+    void Cond::broadcast() {
+        ::pthread_cond_broadcast(&m_hEvent);
+    }
+
+    void Cond::signal() {
+        ::pthread_cond_signal(&m_hEvent);
+    }
+
+    void Cond::wait(Mutex &m) {
+        ::pthread_cond_wait(&m_hEvent, &m.m_hMutex);
+    }
+/******************************操作包裹类**********************************************************/
+    /*
+     * Mutex_scope_lock
+     */
+    Mutex_scope_lock::Mutex_scope_lock(Mutex &m):m_Mutex(m) {
+        m_Mutex.lock();
+    }
+
+    Mutex_scope_lock::~Mutex_scope_lock(Mutex &m) {
+        m_Mutex.unlock();
+    }
+
+    /*
+     * RWLock_scope_rdlock
+     */
+    RWLock_scope_rdlock::RWLock_scope_rdlock(RWLock &m) :m_RwLock(m){
+        m_RwLock.rdlock();
+    }
+
+    RWLock_scope_rdlock::~RWLock_scope_rdlock(RWLock &m) {
+        m_RwLock.unlock();
+    }
+
+    /*
+     * RWLock_scope_wrlock
+     */
+    RWLock_scope_wrlock::RWLock_scope_wrlock(RWLock &m):m_RwLock(m) {
+        m_RwLock.wrlock();
+    }
+
+    RWLock_scope_wrlock::~RWLock_scope_wrlock(RWLock &m) {
+        m_RwLock.unlock();
+    }
+
+///////////////////////////////////////////////////////////////////////
+Thread::Thread(const std::string &name, bool joinable) :threadName(name),alive(false),complete(false),joinable(joinable){
+        m_hThread = 0;
+}
+
+Thread::~Thread() {
+
+}
+
+void Thread::sleep(const long sec) {
+    ::sleep(sec * 1000);
+}
+
+void Thread::msleep(const long msec) {
+    ::sleep(msec);
+}
+
+void Thread::usleep(const long usec) {
+    ::sleep(usec/1000);
+}
+
+/*
+ * 检查线程是否joinable
+ * @return joinable
+ */
+const bool Thread::isJoinable() const {
+    return joinable;
+}
+
+/*
+ * 检查线程是否运行
+ * @return alive
+ */
+const bool Thread::isAlive() const {
+        return alive;
+}
+
+/*
+ * 主动结束线程
+ * 只是设置标志 当线程run 主回调循环检查这个标志,如果这标志已经设置 则退出线程
+ */
+void Thread::final() {
+    complete = true;
+}
+
+const bool Thread::isFinal() const {
+    return complete;
+}
+
+const std::string& Thread::getThreadName() const {
+    return threadName;
+}
+
+void* Thread::threadFunc(void *arg) {
+    Thread *thread = (Thread*)arg;
+
+    thread->m_Lock.lock();
+    thread->alive = true;
+    thread->cond.broadcast();
+    thread->m_Lock.unlock();
+
+
+}
+
+
+
+
 }
--- thread.h
+++ thread.h
@@ -16,7 +16,7 @@
 #include <sys/_types/_pthread_rwlock_t.h>
 #include <sys/_types/_pthread_t.h>
 
-#define MY_HANDLE pthread_t;  //
+#define MY_HANDLE pthread_t
 #define MY_API
 #define MY_MUTEX_HANDLE pthread_mutex_t
 #define MY_COND_HANDLE  pthread_cond_t
@@ -140,6 +140,25 @@ private:
 
 };
 
+class RWLock_scope_wrlock{
+public:
+    /*
+     * 构造函数
+     * 对读写锁进行 wrlock 操作
+     * @param m 读写锁引用
+     */
+    RWLock_scope_wrlock(RWLock &m);
+
+    /*
+     * 析构函数
+     * 对读写锁进行 unlock 操作
+     */
+    ~RWLock_scope_wrlock(RWLock &m);
+private:
+    RWLock &m_RwLock; //读写锁引用
+
+};
+
 /*
  * 封装系统条件变量 省去手工初始化 和 销毁的繁琐工作
  */
@@ -165,13 +184,126 @@ public:
      */
     void signal();
 
+    /*
+     * 等待特定的条件变量满足
+     * @param m 需要等待的互斥体
+     */
+    void wait(Mutex &m);
+
 private:
     MY_COND_HANDLE m_hEvent; //系统条件变量
-
-};
-class thread {
-
 };
 
+class Thread{
+public:
+    /*
+     * 构造函数 创建一个对象
+     * @param name 线程名称
+     * @param joinable 标明此线程退出时 是否保存状态 true 保存 false 不保存
+     */
+    Thread(const std::string &name = std::string("lThread"), bool joinable = true);
+
+    /*
+     * 析构函数 用于销毁一个对象 获取释放的空间
+     */
+    virtual ~Thread();
+
+    /*
+     * 让线程睡眠指定的时间 秒
+     * @param sec 指定时间 秒
+     */
+    static void sleep(const long sec);
+
+    /*
+     * 让线程睡眠指定的时间 毫秒
+     * @param msec 指定时间 毫秒
+     */
+    static void msleep(const long msec);
+
+    /*
+     * 让线程睡眠指定的时间 微秒
+     * @param usec 指定时间 微秒
+     */
+    static void usleep(const long usec);
+
+    /*
+     * 是否保存状态
+     * @return 是否保存
+     */
+    const bool isJoinable() const ;
+
+    /*
+     * 检查线程是否在运行状态
+     * @return 是否在运行
+     */
+    const bool isAlive() const ;
+
+    /*
+
+     */
+    static void *threadFunc(void *arg);
+
+    bool start();
+    bool join();
+
+    /*
+     * 主动结束线程
+     * 设置标志 , 当线程的run 主回调循环的时候遇到这个标志被设置的时候 退出循环
+     */
+    void final();
+
+    /*
+     * 判断线程是否继续执行下去
+     * @return 标志
+     */
+    const bool isFinal() const ;
+
+    /*
+     * 纯虚函数 线程主回调函数
+     * 如果是无限循环 则在每个循环中 都要检查isFinal 这样保证线程安全退出
+     */
+    virtual void run() = 0;
+
+    /*
+     * 返回线程名称
+     */
+    const std::string &getThreadName() const ;
+public:
+    std::string threadName; //线程名称
+    Mutex m_Lock;//互斥锁
+    volatile bool alive;//线程是否在进行
+    volatile bool complete;
+    MY_HANDLE m_hThread;//线程编号
+    bool joinable;//线程标志
+    Cond m_Cond;
+};
+
+class ThreadGroup{
+public:
+    struct stCallback{
+        virtual void *exec(Thread *e) = 0;
+        virtual ~stCallback();
+    };
+
+    typedef std::vector<Thread*> Container ; //容器类型
+
+    ThreadGroup();
+    ~ThreadGroup();
+    void add(Thread *thread);
+    Thread *getByIndex(Container::size_type index);
+    Thread *operator[] (Container::size_type index);
+    void joinAll();
+    void execAll(stCallback &cb);
+
+    const Container::size_type size()
+    {
+         RWLock_scope_rdlock scope_rdlock(m_hRWLock);
+        return m_Cts.size();
+    }
+
+private:
+    Container m_Cts;
+    RWLock m_hRWLock;
+};
 }
 #endif //__thread_H_
