90b28a420b9e609e314e0ed6ff3482bbb2d3671a
lecai <Admin@lecais-MacBook-Pro.local>
2013-11-24 00:52:27 +0800

    socket module
    impl base socket module

--- mac_net.h
+++ mac_net.h
@@ -10,7 +10,61 @@
 #ifndef __mac_net_H_
 #define __mac_net_H_
 
+#include <vector>
+#include <string>
+#include <list>
 
+namespace net{
+    /********************二进制缓存器
+     * Buffer 获取信息后 重置当前位置
+     */
+    class Record{
+    public:
+        Record(void *cmd, unsigned int len)
+        {
+            contents = new unsigned char[len];
+            memcpy(contents, cmd, len);
+            contentSize = len;
+            offset = 0;
+        }
+        Record()
+        {
+            offset = contentSize = 0;
+            contents = NULL;
+        }
+        ~Record()
+        {
+            if(contents) delete contents;
+            contents = NULL;
+        }
+
+        bool empty()
+        {
+            return offset == contentSize;
+        }
+        unsigned int recv(void *buffer,unsigned int len)
+        {
+            if(empty()) return 0;
+            len = leftSize() < len ? leftSize() : len;
+            memcpy(buffer, contents+offset, len);
+            offset+=len;
+            return len;
+        }
+        /*
+         * 读取后剩下长度
+         */
+        unsigned int leftSize()
+        {
+            return contentSize - offset;
+        }
+    private:
+        unsigned int offset;
+        unsigned int contentSize;
+        unsigned char *contents;
+    };
+
+
+}
 class mac_net {
 
 };
