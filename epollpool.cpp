cf052b30d21b3d4216a4c5393c23fff800ff3ec9
lecai <Admin@lecais-MacBook-Pro.local>
2013-12-02 12:23:28 +0800

    epoll thread pool

---
+++ epollpool.cpp
@@ -0,0 +1,6 @@
+//
+// Created by lecai on 13-12-2 下午12:18.
+//
+
+
+#include "epollpool.h"
---
+++ epollpool.h
@@ -0,0 +1,19 @@
+//
+// Created by lecai on 13-12-2 下午12:18.
+// 实现简单轻量级的epoll 连接 线程管理 参考muoduo 多线程网络库 one loop per thread
+// 单线程接受 工作
+
+
+
+#ifndef __epollpool_H_
+#define __epollpool_H_
+
+class ListenerThread; //监听线程
+class RecycleThread;  //回收线程
+class WorkThread;     //工作线程
+class epollpool {
+
+};
+
+
+#endif //__epollpool_H_
